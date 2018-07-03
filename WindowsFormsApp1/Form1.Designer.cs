19f66565e634f57f6c92faaf0d6943151a204e6d
lurium-ruri <luriumruri@gmail.com>
2018-07-03 14:41:46 +0900

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.vScrollBar1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 426);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.flowLayoutPanel1);
+            this.panel2.Location = new System.Drawing.Point(3, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(678, 426);
+            this.panel2.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(-4, -1);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(678, 0);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(681, 0);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(95, 426);
+            this.vScrollBar1.TabIndex = 1;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private static readonly int PICTURE_WIDTH = 678;
+        private static readonly int PICTURE_HEIGHT = 1080;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //picture読み込み
+            string[] files = System.IO.Directory.GetFiles(@".\img", "*");
+            Array.Sort(files, new NaturalStringCompare());
+            var height = 0;
+            foreach (string filePath in files)
+            {
+                var pictureBox = new PictureBox
+                {
+                    Image = new Bitmap(filePath),
+                    Location = new System.Drawing.Point(0, 0),
+                    Name = "pictureBox1",
+                    Size = new System.Drawing.Size(PICTURE_WIDTH, PICTURE_HEIGHT),
+                    Margin = new Padding(0, 0, 0, 0)
+                };
+                this.flowLayoutPanel1.Controls.Add(pictureBox);
+                height += PICTURE_HEIGHT;
+            }
+            var width = flowLayoutPanel1.Size.Width;
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(width, height);
+        }
+
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            int x = this.flowLayoutPanel1.Location.X;
+            double y = e.NewValue / 100.0 * this.flowLayoutPanel1.Height;
+
+            this.flowLayoutPanel1.Location = new Point(x, -(int)y);
+            this.flowLayoutPanel1.Refresh();
+        }
+    }
+
+    class NaturalStringCompare : IComparer<string>
+    {
+        int IComparer<string>.Compare(string x, string y)
+        {
+            // 名称の比較
+            return NaturalCompare(x, y);
+        }
+
+        private int NaturalCompare(string x, string y)
+        {
+            if (x == y)
+            {
+                return 0;
+            }
+
+            var ix = 0;
+            var iy = 0;
+
+            if (int.TryParse(x, out ix) && int.TryParse(y, out iy))
+            {
+                return ix.CompareTo(iy);
+            }
+
+            var xs = Regex.Split(x.Replace(" ", ""), @"(\d+)");
+            var ys = Regex.Split(y.Replace(" ", ""), @"(\d+)");
+
+            if (xs.Length == 1 && ys.Length == 1)
+            {
+                return x.CompareTo(y);
+            }
+
+            var minLength = Math.Min(xs.Length, ys.Length);
+
+            foreach (var i in Enumerable.Range(0, minLength))
+            {
+                if (xs[i] != ys[i])
+                {
+                    return NaturalCompare(xs[i], ys[i]);
+                }
+            }
+
+            return xs.Length.CompareTo(ys.Length);
+        }
+    }
+}
